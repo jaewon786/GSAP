caf64d8f226df777b0bcc147437e3a9a38f48c90
jaewon <jaewono786@gmail.com>
2025-01-06 14:01:24 +0900

    추천, 믹스 추가

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 
-const ani = gsap.timeline();
-ani.from(".section-every", {
+const sec1 = gsap.timeline();
+sec1.from(".section-every", {
   autoAlpha: 0,
   y: -100,
   stagger: {
-    amount: 0.3,
+    amount: 0.2,
     from: "random"
   }
-})
+});
 
 ScrollTrigger.create({
-  animation: ani,
+  animation: sec1,
   trigger: "#section1",
   start: "top 10%",
-  anticipatePin: 1,
-  markers: true
+});
+
+const sec2 = gsap.timeline();
+sec2.from(".section-recommend", {
+  autoAlpha: 0,
+  x: -100,
+  duration: 1
+});
+
+ScrollTrigger.create({
+  animation: sec2,
+  trigger: "#section2",
+  start: "top 30%",
+});
+
+const sec3 = gsap.timeline();
+sec3.from(".section-mix", {
+  autoAlpha: 0,
+  x: 100,
+  duration: 1
+});
+
+ScrollTrigger.create({
+  animation: sec3,
+  trigger: "#section3",
+  start: "top 60%",
 });
